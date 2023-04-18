48a40dd49e3bcf559cec8344da4ca23e86034f7e
leebyungjik <aloepig@gmail.com>
2023-04-18 15:07:02 +0900

    ip 추출하기

--- elblog/logfile_ip.py
+++ elblog/logfile_ip.py
@@ -30,14 +30,23 @@ folder_path = "/Users/leebyungjik/s3backup"
 # 폴더 안의 파일을 리스트로 저장합니다.
 file_list = os.listdir(folder_path)
 
-ip_array = []
+ip_set = {1}
+
+Array_one = []
 
 for log_file in file_list:
     print(log_file)
     if ".DS_Store" != log_file:
         file_path = os.path.join(folder_path, log_file)
         # 파일을 읽어와서 출력합니다.
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-            for log in lines:
-                print(extract_ip_address(log))
+        try:
+            with open(file_path, "r") as file:
+                lines = file.readlines()
+                for log in lines:
+                    if len(log) > 1:
+                        # print(extract_ip_address(log))
+                        ip_set.add(extract_ip_address(log)[0])
+        except:
+            print("error")
+        finally:
+            print(ip_set)
