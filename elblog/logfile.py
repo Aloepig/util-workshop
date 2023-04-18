989667339d65b13a22953f9e13cbbe86d1c6cfb7
leebyungjik <aloepig@gmail.com>
2023-04-18 14:48:00 +0900

    ip 추출하기

--- elblog/logfile.py
+++ elblog/logfile.py
@@ -1,7 +1,7 @@
 import os
 
 # 폴더 경로와 파일 개수를 지정합니다.
-folder_path = "/Users/leebyungjik/s3backup/aws-logs-elb/fancast-api/AWSLogs/690116638181/elasticloadbalancing/ap-northeast-2/2023/04/18"
+folder_path = "/Users/leebyungjik/s3backup/aws-logs-elb/fancast-api/AWSLogs/690116638181/elasticloadbalancing/ap-northeast-2/2023/04/205"
 
 # 폴더 안의 파일을 리스트로 저장합니다.
 file_list = os.listdir(folder_path)
@@ -10,9 +10,9 @@ file_list = os.listdir(folder_path)
 # filename = "690116638181_elasticloadbalancing_ap-northeast-2_app.FANCAST-API.f638a8a6bfb2fb6d_20230418T0300Z_15.165.184.157_32lqywfm.log"
 
 for log_file in file_list:
-    print("===========")
-    print(log_file)
-    print("===========")
+    # print("===========")
+    # print(log_file)
+    # print("===========")
     file_path = os.path.join(folder_path, log_file)
     # 파일을 읽어와서 출력합니다.
     with open(file_path, "r") as file:
---
+++ elblog/logfile_ip.py
@@ -0,0 +1,43 @@
+import os
+import re
+
+
+# 문자열에서 IP 주소를 추출하는 함수를 정의합니다.
+def extract_ip_address(string):
+    # IP 주소를 추출하는 정규식 패턴을 지정합니다.
+    pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+
+    # 정규식 패턴과 일치하는 문자열을 모두 추출합니다.
+    matches = re.findall(pattern, string)
+
+    # 추출한 IP 주소 리스트를 반환합니다.
+    return matches
+
+
+# 예시 문자열을 지정합니다.
+# string = "This is an example text with IP address 192.168.0.1 and 10.0.0.1"
+
+# 문자열에서 IP 주소를 추출합니다.
+# ip_addresses = extract_ip_address(string)
+
+# 추출된 IP 주소를 출력합니다.
+# print(ip_addresses)
+
+
+# 폴더 경로와 파일 개수를 지정합니다.
+folder_path = "/Users/leebyungjik/s3backup"
+
+# 폴더 안의 파일을 리스트로 저장합니다.
+file_list = os.listdir(folder_path)
+
+ip_array = []
+
+for log_file in file_list:
+    print(log_file)
+    if ".DS_Store" != log_file:
+        file_path = os.path.join(folder_path, log_file)
+        # 파일을 읽어와서 출력합니다.
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            for log in lines:
+                print(extract_ip_address(log))
